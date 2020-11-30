298ae8a8768888f777295da0932bbd408dde3963
sagar.giri <sagar.giri@bebit.com>
2020-11-30 21:54:25 +0900

    FIX: Add missing constants

---
+++ kodomath/mathops/constants.py
@@ -0,0 +1,3 @@
+BEGINNER_SCORE = 8  # Since no. of questions in beginner is 2
+MEDIUM_SCORE = 12  # BEGINNER_SCORE + no. of questions in medium level
+ADVANCED_SCORE = 16  # MEDIUM_SCORE + no. of questions in advanced level
