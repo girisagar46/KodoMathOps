6595dba8a49a7db6c6bce77bffcd041a5c76dd34
sagar.giri <sagar.giri@bebit.com>
2020-11-30 16:12:53 +0900

    [KOD-16] Django models for Question, StudentStats and Tracking questions answered by students
    Fixes: #19

---
+++ kodomath/mathops/models.py
@@ -0,0 +1,43 @@
+import uuid
+from datetime import timedelta
+
+from django.conf import settings
+from django.db import models
+
+
+class Level(models.IntegerChoices):
+    BEGINNER = 1, "Beginner"
+    MEDIUM = 2, "Medium"
+    ADVANCED = 3, "Advanced"
+    COMPLETED = 4, "Completed"
+
+
+class StudentStats(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    level = models.PositiveIntegerField(
+        "Level", default=Level.BEGINNER, choices=Level.choices
+    )
+
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    updated_at = models.DateTimeField(editable=False, auto_now=True)
+
+
+class QuestionBank(models.Model):
+    id = models.IntegerField(primary_key=True, editable=False)
+    level = models.PositiveIntegerField(
+        "Level", default=Level.BEGINNER, choices=Level.choices
+    )
+    question = models.JSONField(verbose_name="question", blank=True, default=dict)
+
+
+class QuestionTracker(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    player_stats = models.ForeignKey(StudentStats, on_delete=models.DO_NOTHING)
+    question_bank = models.ForeignKey(QuestionBank, on_delete=models.DO_NOTHING)
+
+    time_to_answer = models.DurationField(default=timedelta())
+    submitted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    updated_at = models.DateTimeField(editable=False, auto_now=True)
--- kodomath/users/models.py
+++ kodomath/users/models.py
@@ -1,10 +1,13 @@
 import uuid
-from django.db import models
+
 from django.conf import settings
-from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+
+from kodomath.mathops.models import StudentStats
 
 
 class User(AbstractUser):
@@ -18,3 +21,9 @@ class User(AbstractUser):
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_player_stat(sender, instance=None, created=False, **kwargs):
+    if created:
+        StudentStats.objects.create(user=instance)
